f8175c5a4ccf5e9e4646291db77e46179ff49d26
Eun Oh <bdohhhhh@gmail.com>
2025-05-04 18:23:42 +0900

    [update]: backtask 완성

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -1,51 +1,74 @@
 import { createServerClient } from "@supabase/ssr";
 import { type NextRequest, NextResponse } from "next/server";
 
+const allowedOrigins = [
+  "https://iffygiftyday.vercel.app",
+  "https://main.d37yd0e7u56dxt.amplifyapp.com",
+  "http://localhost:3000",
+];
+
+const corsOptions = {
+  "Access-Control-Allow-Methods": "GET",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
 export async function updateSession(request: NextRequest) {
-	let supabaseResponse = NextResponse.next({
-		request,
-	});
+  let supabaseResponse = NextResponse.next({
+    request,
+  });
 
-	const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-	const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+  // Check the origin from the request
+  const origin = request.headers.get("origin") ?? "";
+  const isAllowedOrigin = allowedOrigins.includes(origin);
 
-	if (!supabaseUrl || !supabaseAnonKey) {
-		throw new Error("Missing Supabase URL or Anon Key");
-	}
+  if (isAllowedOrigin) {
+    supabaseResponse.headers.set("Access-Control-Allow-Origin", origin);
+  }
 
-	const supabase = createServerClient(supabaseUrl, supabaseAnonKey, {
-		cookies: {
-			getAll() {
-				return request.cookies.getAll();
-			},
-			setAll(cookiesToSet) {
-				// biome-ignore lint/complexity/noForEach: <explanation>
-				cookiesToSet.forEach(({ name, value, options }) =>
-					request.cookies.set(name, value),
-				);
-				supabaseResponse = NextResponse.next({
-					request,
-				});
-				// biome-ignore lint/complexity/noForEach: <explanation>
-				cookiesToSet.forEach(({ name, value, options }) =>
-					supabaseResponse.cookies.set(name, value, options),
-				);
-			},
-		},
-	});
+  for (const [key, value] of Object.entries(corsOptions)) {
+    supabaseResponse.headers.set(key, value);
+  }
 
-	// refreshing the auth token
-	// const {
-	// 	data: { user },
-	// } = await supabase.auth.getUser();
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-	// console.log("user =====>", user);
+  if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error("Missing Supabase URL or Anon Key");
+  }
 
-	// if (!user) {
-	// 	const url = request.nextUrl.clone();
-	// 	url.pathname = "/signin";
-	// 	return NextResponse.redirect(url);
-	// }
+  const supabase = createServerClient(supabaseUrl, supabaseAnonKey, {
+    cookies: {
+      getAll() {
+        return request.cookies.getAll();
+      },
+      setAll(cookiesToSet) {
+        // biome-ignore lint/complexity/noForEach: <explanation>
+        cookiesToSet.forEach(({ name, value, options }) =>
+          request.cookies.set(name, value)
+        );
+        supabaseResponse = NextResponse.next({
+          request,
+        });
+        // biome-ignore lint/complexity/noForEach: <explanation>
+        cookiesToSet.forEach(({ name, value, options }) =>
+          supabaseResponse.cookies.set(name, value, options)
+        );
+      },
+    },
+  });
 
-	return supabaseResponse;
+  // refreshing the auth token
+  // const {
+  // 	data: { user },
+  // } = await supabase.auth.getUser();
+
+  // console.log("user =====>", user);
+
+  // if (!user) {
+  // 	const url = request.nextUrl.clone();
+  // 	url.pathname = "/signin";
+  // 	return NextResponse.redirect(url);
+  // }
+
+  return supabaseResponse;
 }
